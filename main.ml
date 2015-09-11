deca76f276dca14b4b69fe6df635753facccbeca
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-09-11 13:07:37 +0900

    Error handling

--- main.ml
+++ main.ml
@@ -1,7 +1,7 @@
 open Syntax
 open Eval
 
-let rec read_eval_print env =
+let read_eval_print env =
   print_string "# ";
   flush stdout;
   let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
@@ -9,15 +9,35 @@ let rec read_eval_print env =
   Printf.printf "val %s : " id;
   pp_val v;
   print_newline();
-  read_eval_print newenv
+  newenv
 ;;
 
-let initial_env =
-  Environment.extend "i" (IntV 1)
-    (Environment.extend "v" (IntV 5)
-      (Environment.extend "x" (IntV 10) Environment.empty))
+let rec read_eval_print_loop env =
+  let newenv = 
+    try read_eval_print env with
+      Eval.Error msg -> Printf.printf "Error: %s\n" msg; env
+    | Parsing.Parse_error -> print_string "Error: Parse Error\n"; env
+    | Failure msg -> Printf.printf "Error: %s\n" msg; env
+  in
+  read_eval_print_loop newenv
 ;;
 
-let _ = read_eval_print initial_env
+let rec make_env = function
+    [] -> Environment.empty
+  | (id, v)::rest -> Environment.extend id v (make_env rest)
 ;;
+
+let roman = [
+    ("i",   IntV 1);
+    ("ii",  IntV 2);
+    ("iii", IntV 3);
+    ("iv",  IntV 4);
+    ("v",   IntV 5);
+    ("x",   IntV 10);
+    ]
+;;
+
+let initial_env = make_env roman
+
+let _ = read_eval_print_loop initial_env
 
